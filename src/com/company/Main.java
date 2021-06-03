380c4d257b18b08fbdd28aad65bdf7030404be0e
Weedy <dkeziks@yahoo.com>
2021-06-03 11:52:40 +0300

    Initial Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        switch (s) {
+            case "lol":
+                System.out.println("LOL");
+        }
+    }
+}
